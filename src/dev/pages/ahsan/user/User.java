e5d0ef69b79d88b730bf64043f30debc40948b5c
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-19 14:12:30 +0600

    updated user class

--- src/dev/pages/ahsan/user/User.java
+++ src/dev/pages/ahsan/user/User.java
@@ -65,4 +65,12 @@ public class User implements Serializable {
     public void setPasswords(String passwords) {
         this.passwords = passwords;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
