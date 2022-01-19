aaa6ff8d593b1d521aefdcbf39e27472e2a4aadf
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-19 15:53:26 +0600

    updated ticket class

--- src/dev/pages/ahsan/user/Ticket.java
+++ src/dev/pages/ahsan/user/Ticket.java
@@ -3,7 +3,6 @@ package dev.pages.ahsan.user;
 import java.time.LocalDate;
 
 public class Ticket {
-    String uid;
     String from;
     String to;
     LocalDate date;
@@ -11,13 +10,60 @@ public class Ticket {
     int seat;
     int bus;
 
-    public Ticket(String uid, String from, String to, LocalDate date, String time, int seat, int bus) {
-        this.uid = uid;
+    public Ticket(String from, String to, LocalDate date, String time, int seat, int bus) {
         this.from = from;
         this.to = to;
         this.date = date;
         this.time = time;
         this.seat = seat;
+        this.bus = bus;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
+    public int getBus() {
+        return bus;
+    }
+
+    public void setBus(int bus) {
         this.bus = bus;
     }
 }
